1997135e64952bbe4ec676bf4b885c0e9f3d46e1
Leyson Fabian Castaño Perez <leyson.castano@misena.edu.com>
2025-04-23 09:33:03 -0500

    Cargue a repositorio

--- src/config/global.js
+++ src/config/global.js
@@ -78,7 +78,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/86120364_CF04_DU.pdf',
       },
       {
         icono: 'fas fa-download',
